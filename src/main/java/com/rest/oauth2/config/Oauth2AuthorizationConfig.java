2c8e5f580ab0686c42677b2e0cf37a9d5cd20589
kimyonghwa <codej99@naver.com>
2019-04-24 02:25:12 +0900

    Apply accessTokenConverter
    - JWT TokenConverter

--- src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
@@ -2,6 +2,7 @@ package com.rest.oauth2.config;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 import javax.sql.DataSource;
 
@@ -42,34 +44,31 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
      *
      * @return
      */
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.tokenStore(new JdbcTokenStore(dataSource));
-    }
-
-
-//
-//    /**
-//     * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
-//     *
-//     * @param endpoints
-//     * @throws Exception
-//     */
 //    @Override
 //    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-//        super.configure(endpoints);
-//        endpoints.accessTokenConverter(jwtAccessTokenConverter());
+//        endpoints.tokenStore(new JdbcTokenStore(dataSource))
+//                .accessTokenConverter(jwtAccessTokenConverter());
 //    }
-//
 
-//
-//    /**
-//     * jwt converter를 등록
-//     *
-//     * @return
-//     */
-//    @Bean
-//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-//        return new JwtAccessTokenConverter();
-//    }
+    /**
+     * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
+     *
+     * @param endpoints
+     * @throws Exception
+     */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        super.configure(endpoints);
+        endpoints.accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    /**
+     * jwt converter를 등록
+     *
+     * @return
+     */
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        return new JwtAccessTokenConverter();
+    }
 }
