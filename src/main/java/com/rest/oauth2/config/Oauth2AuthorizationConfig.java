f0d81468146483bce87a4f4141208c059e7b114a
kimyonghwa <codej99@nhnent.com>
2019-04-25 18:15:22 +0900

    allow /oauth/check_token

--- src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
+++ src/main/java/com/rest/oauth2/config/Oauth2AuthorizationConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
@@ -34,6 +35,13 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     @Value("${oauth2.jwt.signkey}")
     private String signKey;
 
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()") //allow check token
+                .allowFormAuthenticationForClients();
+    }
+
     /**
      * 클라이언트 정보 주입 방식을 jdbcdetail로 변경
      *
@@ -50,10 +58,10 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
      *
      * @return
      */
-//    @Override
-//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-//        endpoints.tokenStore(new JdbcTokenStore(dataSource)).userDetailsService(userDetailService);
-//    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.tokenStore(new JdbcTokenStore(dataSource)).userDetailsService(userDetailService);
+    }
 
     /**
      * 토큰 발급 방식을 JWT 토큰 방식으로 변경한다. 이렇게 하면 토큰 저장하는 DB Table은 필요가 없다.
@@ -61,21 +69,21 @@ public class Oauth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
      * @param endpoints
      * @throws Exception
      */
-    @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        super.configure(endpoints);
-        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
-    }
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        super.configure(endpoints);
+//        endpoints.accessTokenConverter(jwtAccessTokenConverter()).userDetailsService(userDetailService);
+//    }
 
     /**
      * jwt converter를 등록
      *
      * @return
      */
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(signKey);
-        return converter;
-    }
+//    @Bean
+//    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        converter.setSigningKey(signKey);
+//        return converter;
+//    }
 }
